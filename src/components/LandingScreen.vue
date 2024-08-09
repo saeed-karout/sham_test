4ee0dd00e6d9be764cee62001cd60eb225183206
saeed-karout <saed.karot1@gmail.com>
2024-08-09 15:40:29 +0300

    update color landing page

--- src/components/LandingScreen.vue
+++ src/components/LandingScreen.vue
@@ -4,9 +4,9 @@
   <div class='flex space-x-2 justify-center items-center  h-screen  dark:bg-slate-800'>
  	
  	<span class='sr-only'>...</span>
-  	<div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-	<div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-	<div class='h-8 w-8 bg-black rounded-full animate-bounce'></div>
+  	<div class='h-8 w-8 bg-[#B99269]  rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+	<div class='h-8 w-8 bg-[#B99269] rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+	<div class='h-8 w-8 bg-[#B99269] rounded-full animate-bounce'></div>
 </div>
 </template>
 
